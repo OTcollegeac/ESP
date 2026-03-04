6c31a7f5d0f1c836546896c511778ffe197ee847
OTcollegeac <138622@student.grantham.ac.uk>
2026-03-04 13:52:10 +0000

    The robots are taking over

--- main.py
+++ main.py
@@ -49,49 +49,31 @@ def GetSalesPersonData(Id):
 
 def GetGoat():#This function reads the csv and determines the highest value (number of sales) a person has
     df=pd.read_csv('data.csv')
-    data=[]#initalises the data list
-    for index in range(len(df)):#loops through dataframe in order to create a list with the summed values of each persons sales
-        data.append( df.iloc[index,4:].sum())
-    win=max(data)#this retrives the highest value in the list
-    for index in range(len(df)): #This loop is needed to search through the data frame to find which row the greatest value is on to present the best salesperson
-        if df.iloc[index,4:].sum() == win: 
-            #print(f'Best Salesperson is at row {index}')
-            Fname=df.loc[index,"Forename"]
-            Lname=df.loc[index,"Surname"]
-            print(f'The best Sales Person of all time is {Fname} {Lname} with {win} Sales.')
+    df=ComputeTotalSales(df)
+    print(f'The Best Sales Person of all time is {df["Total Sales"].idxmax()} with {df["Total Sales"].max()} Sales.')
+
+#    data=[]#initalises the data list
+ #   for index in range(len(df)):#loops through dataframe in order to create a list with the summed values of each persons sales
+  #      data.append( df.iloc[index,4:].sum())
+   # win=max(data)#this retrives the highest value in the list
+   # for index in range(len(df)): #This loop is needed to search through the data frame to find which row the greatest value is on to present the best salesperson
+   #     if df.iloc[index,4:].sum() == win: 
+   #         #print(f'Best Salesperson is at row {index}')
+   #         Fname=df.loc[index,"Forename"]
+   #         Lname=df.loc[index,"Surname"]
+  #          print(f'The best Sales Person of all time is {Fname} {Lname} with {win} Sales.')
 
     
     
-def GetBestReigon():
+def GetBestReigon(): #The following function was assisted by AI
     df=pd.read_csv('data.csv')
-    data=[]#initalises the data list
-    London=0
-    Wales=0
-    SouthWest=0
-    SouthEast=0
-    WestMidlands=0
-    for index in range(len(df)):
-        data.append( df.iloc[index,4:].sum())
-        #for index in range(len(df)): 
-        currentValue=data[index]
-        if df.iloc[index,4:].sum() == currentValue: 
-                if df.loc[index,"Region"]=='London':
-                    London+=currentValue
-                elif df.loc[index,"Region"]=='Wales':
-                    Wales+=currentValue
-                elif df.loc[index,"Region"]=='South-West':
-                    SouthWest+=currentValue
-                elif df.loc[index,"Region"]=='South-East':
-                    SouthEast+=currentValue
-                elif df.loc[index,"Region"]=='West-Midlands':
-                    WestMidlands+=currentValue
-                print(max(London,Wales,SouthEast,SouthWest,WestMidlands))#Finds the highest value from all these reigons
-                
-                
+    df=ComputeTotalSales(df)
+    regionTotals=df.groupby('Region')['Total Sales'].sum()
+    print(f"The best performing region is {regionTotals.idxmax()} with {regionTotals.max()} total sales.")
 
-
-
-
+def ComputeTotalSales(df):
+    df['Total Sales']=df.iloc[:,4:].sum(axis=1)
+    return df
 
 while True:
     menu()
